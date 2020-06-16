5a67a54db8d54d3acf7e8fa623dc447f3a459874
zhoudejie <zhoudj94@gmail.com>
2020-06-16 11:24:28 +0800

    韵脚记录更详细

--- index.js
+++ index.js
@@ -1,78 +1,99 @@
-const { load, cut } = require("@node-rs/jieba");
-const fs = require("fs");
-let axios = require("axios");
-let 词频 = {};
-let 词频数组 = [];
-let 尾词频 = {};
-let 尾词频数组 = [];
-let 无理关键字 = /:|\)|）|　|？|\./g;
+const { load, cut } = require('@node-rs/jieba')
+const fs = require('fs')
+let axios = require('axios')
+let 词频 = {}
+let 词频数组 = []
+let 韵脚 = {}
+let 韵脚数组 = []
+let 无理关键字 = /:|\)|）|　|？|\./g
 async function 单曲词频分析(id) {
-  let lyric = (
-    await axios.get("https://api.imjad.cn/cloudmusic/?type=lyric&id=" + id)
-  ).data.lrc.lyric;
-  lyric = lyric
-    .replace(/\[.+\]/g, "")
-    .replace(/\n/g, " ")
-    .split(" ")
-    .map((v) => {
+  let exist = await 检测文件是否存在(id)
+  let lyric = ''
+  if (exist) {
+    lyric = fs.readFileSync('./cache/' + id, 'utf8')
+  } else {
+    lyric = (
+      await axios.get('https://api.imjad.cn/cloudmusic/?type=lyric&id=' + id)
+    ).data.lrc.lyric
+    fs.writeFileSync('./cache/' + id, lyric, 'utf8')
+  }
+  lyric
+    .replace(/\[.+\]/g, '')
+    .split('\n')
+    .map((v, line) => {
+      // TODO 空格断为两句
       if (v) {
-        let 分词 = cut(v);
-        let lastItem = 分词[分词.length - 1];
+        let 分词 = cut(v)
+        let lastItem = 分词[分词.length - 1]
         if (lastItem.match(无理关键字)) {
-          lastItem = 分词[分词.length - 2];
+          lastItem = 分词[分词.length - 2]
         }
         if (lastItem) {
-          if (尾词频[lastItem]) {
-            尾词频[lastItem] += 1;
+          if (韵脚[lastItem]) {
+            韵脚[lastItem].push({ id, line })
           } else {
-            尾词频[lastItem] = 1;
+            韵脚[lastItem] = [{ id, line }]
           }
         }
         for (let i = 0; i < 分词.length; i++) {
-          let item = 分词[i];
+          let item = 分词[i]
           if (item.match(无理关键字)) {
-            continue;
+            continue
           }
           if (词频[item]) {
-            词频[item] += 1;
+            词频[item] += 1
           } else {
-            词频[item] = 1;
+            词频[item] = 1
           }
         }
       }
-      return v;
-    });
-  console.log(id + "分析完成");
+      return v
+    })
+  console.log(id + '分析完成')
 }
 
 async function 歌单词频分析(id) {
   let list = (
-    await axios.get("https://api.imjad.cn/cloudmusic/?type=playlist&id=" + id)
-  ).data.playlist.trackIds;
+    await axios.get('https://api.imjad.cn/cloudmusic/?type=playlist&id=' + id)
+  ).data.playlist.trackIds
 
   for (let i = 0; i < list.length; i++) {
-    await 单曲词频分析(list[i].id);
+    await 单曲词频分析(list[i].id)
   }
   for (const key in 词频) {
-    词频数组.push({ word: key, count: 词频[key] });
+    词频数组.push({ word: key, count: 词频[key] })
   }
-  for (const key in 尾词频) {
-    尾词频数组.push({ word: key, count: 尾词频[key] });
+  for (const key in 韵脚) {
+    韵脚数组.push({ word: key, count: 韵脚[key].length, source: 韵脚[key] })
   }
-  生成文件(id);
+  生成文件(id)
 }
 
 function 生成文件(id) {
   词频数组 = 词频数组.sort((a, b) => {
-    return b.count - a.count;
-  });
-  尾词频数组 = 尾词频数组.sort((a, b) => {
-    return b.count - a.count;
-  });
-  fs.writeFileSync(id + "词频.json", JSON.stringify(词频), "utf8");
-  fs.writeFileSync(id + "尾词频.json", JSON.stringify(尾词频), "utf8");
-  fs.writeFileSync(id + "词频数组.json", JSON.stringify(词频数组), "utf8");
-  fs.writeFileSync(id + "尾词频数组.json", JSON.stringify(尾词频数组), "utf8");
+    return b.count - a.count
+  })
+  韵脚数组 = 韵脚数组.sort((a, b) => {
+    return b.count - a.count
+  })
+  fs.writeFileSync(id + '词频.json', JSON.stringify(词频), 'utf8')
+  fs.writeFileSync(id + '韵脚.json', JSON.stringify(韵脚), 'utf8')
+  fs.writeFileSync(id + '词频数组.json', JSON.stringify(词频数组), 'utf8')
+  fs.writeFileSync(id + '韵脚数组.json', JSON.stringify(韵脚数组), 'utf8')
 }
 
-歌单词频分析(632021463);
+function 检测文件是否存在(id) {
+  return new Promise((resolve, reject) => {
+    fs.access('./cache/' + id, fs.constants.F_OK, (err) => {
+      if (err) {
+        resolve(0)
+      } else {
+        resolve(1)
+      }
+    })
+  })
+}
+
+歌单词频分析(2299383318)
+
+// 检测文件是否存在(1).then(res=>console.log(res))
